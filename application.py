73068a9465cb51a16f6ed4c0b3925778963e383a
yoozafree <144996083+yoozafree@users.noreply.github.com>
2023-10-26 17:32:26 +0900

    Update application.py

--- application.py
+++ application.py
@@ -1,1 +1,7 @@
-
+from flask import Flask
+application = Flask(__name__)
+@application.route("/")
+def hello():
+ return "Hello goorm!"
+if __name__ == "__main__":
+ application.run(host='0.0.0.0', debug=True)
